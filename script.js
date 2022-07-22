3e06abebed73c2f5414973993312fd7aaf1448b4
Damian Padilla <damipad4@gmail.com>
2022-07-22 16:26:17 -0500

    added a few new questions to practice

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Check for JS file link.");
+//Return the sum of Two Number
+const numAdd = (a, b) => a + b;
+
+console.log(numAdd(1, 2));
+
+//Convert Minutes into seconds
+const converter = (a) => {
+    let result = a * 60;
+    console.log(`There are ${result} seconds in ${a} minutes`);
+}
+
+console.log(converter(500));
+
+/* Write a JavaScript program to display the current day and time in the following format:
+ Sample Output:
+ Today is: Tuesday
+ Current time is: 10:30 PM
+*/
+
+
+
+
+// Write a JavaScript program to find the area of a triangle where lengths of the three sides are: 5,6,7
+
+/*  Write a program where the program takes a random integer between 1 to 10 and the user is prompted to input a guess. If the user
+matches with 
+*/
