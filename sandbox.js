2b8767c532a30f5f3805d695076a2ed0305b0734
Damian Padilla <damipad4@gmail.com>
2022-07-27 01:51:54 -0500

    Added new project: Color Flipper

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+const checker = (a, b) => a === 100 || b === 100 || a + b === 100;
+
+console.log(checker(100, 0));
+console.log(checker(0, 100));
+console.log(checker(5, 50));
+console.log(checker(80, 20));
+
+
+const getFileExtension = (str) => str.slice(str.lastIndexOf('.'));
+
+console.log(getFileExtension("index.html"));
+console.log(getFileExtension("webpack.config.js"));
