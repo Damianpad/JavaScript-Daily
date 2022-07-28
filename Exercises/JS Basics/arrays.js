d6821b094b4003f6ea4e73ad255b3dda7385aaae
Damian Padilla <damipad4@gmail.com>
2022-07-28 00:18:49 -0500

    Added new exercises in the Array Basics

--- Exercises/JS Basics/arrays.js
+++ Exercises/JS Basics/arrays.js
@@ -1,5 +1,29 @@
 /* Basics: JavaScript Arrays */
 
-// 1. Write a JavaScript function to get the first element of an array/
+// Create an array named fruits that contains 4 dirrerent types of fruits
+const fruits = ["Apples", "Bananas", "Grapes", "Strawberries"];
 
-const first = (n) => 
+// Print out the "Grapes" element to the console
+console.log(fruits[2]);
+
+// Change the "Bananas" value to "Peach"
+fruits[1] = "Peach";
+console.log(fruits);
+
+// Print the length of the fruits array
+console.log(fruits.length);
+
+// Loop through the fruits array and print out each one to the console
+for (let i = 0; i < fruits.length; i++) {
+  console.log(i);
+}
+
+// Write a JavaScript function that returns the first element of fruits
+
+const first = (n) => {
+  return n[0];
+}
+
+console.log(first(fruits));
+
+//
