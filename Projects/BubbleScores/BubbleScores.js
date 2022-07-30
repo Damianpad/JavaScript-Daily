9ebb4a524626019f0948aeb13f61d1c2687718a2
Damian Padilla <damipad4@gmail.com>
2022-07-29 19:27:12 -0500

    Added README for Bubble Scores and worked on a solution ;)

--- Projects/BubbleScores/BubbleScores.js
+++ Projects/BubbleScores/BubbleScores.js
@@ -1,9 +1,4 @@
-/* Write a program that prints out the bubble solution index# and its score
-Print the total number of scores
-Print the highest scores
-Print the Solutions with the highest scores
-
-Example:
+/* Desired output:
 
 Bubble Solution #0 Score: 60
 Bubble solution #1 Score: 50
@@ -16,13 +11,23 @@ Solutions with highest score: #11, #18
 
 */
 
-const score = [60,50,60,58,54,45,58,50,52,54,48,69,34,55,51,52,44,51,69,64,66,55,52,61,46,31,57,52,44,18,41,53,55,61,51,44];
+const score = [60, 50, 60, 58, 54, 45, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+const highScore = Math.max(...score);
+const highestSolutions = score.indexOf(highScore);
 
 i = 0;
-while(i < score.length) {
+while (i < score.length) {
   output = `Bubble Solution #${i} Score: ${score[i]}`;
   console.log(output)
   i++;
 }
 
-console.log(` Bubble tests: ${score.length}`);
+const maxIndices = () => {
+
+}
+
+console.log(`Bubble tests: ${score.length}`);
+
+
+console.log(`Highest Score: ${highScore}`);
+console.log(`Solutions with highest score: #${highestSolutions}`);
---
+++ Projects/BubbleScores/README.md
@@ -0,0 +1,17 @@
+/* Write a program that prints out the bubble solution index# and its score
+Print the total number of scores
+Print the highest scores
+Print the Solutions with the highest scores
+
+Example:
+
+Bubble Solution #0 Score: 60
+Bubble solution #1 Score: 50
+| Rest of Scores go here
+| Rest of Scores go here
+
+Bubble tests: 36
+Highest Score: 69
+Solutions with highest score: #11, #18
+
+*/
