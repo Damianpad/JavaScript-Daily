2706ea0959317e255d9cd6b679748fc42e1db436
Damian Padilla <damipad4@gmail.com>
2022-08-06 20:30:50 -0500

    Updating DOM.js

--- Exercises/JS Basics/DOM Basics/DOM.js
+++ Exercises/JS Basics/DOM Basics/DOM.js
@@ -11,3 +11,17 @@ querySelectorAll
 getAttribute
 setAttribute
 */
+
+//Using the getElementById selector select and print the milk li
+
+let milk = document.getElementById("milk");
+console.log(milk);
+
+// Using querySelector select and print 
+let apple = document.querySelector("li");
+console.log(apple);
+
+// Using querySelectorAll Select all of the li and h1 in the document
+
+let allElements = document.querySelectorAll("h1", "li");
+console.log(allElements);
