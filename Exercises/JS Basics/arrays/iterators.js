e160e88c98ffa51598faf649ab907020412c6380
Damian Padilla <damipad4@gmail.com>
2022-08-22 22:17:59 -0500

    Added iterators to arrays exercises

---
+++ Exercises/JS Basics/arrays/iterators.js
@@ -0,0 +1,21 @@
+/*
+for ... of
+
+The for ... of statement creates a loop iterating over iterable objects.
+*/
+
+// Use a for of loop to iterate over simpleArray
+
+const simpleArray = [1, 2, 3, 4, 5];
+
+for (const n of simpleArray) {
+    console.log(n);
+}
+
+// Use a for of loop to iterate over nums
+
+const nums = [2, 7, 11, 15];
+
+for (const n of nums) {
+    console.log(n);
+}
